4e713c919b84a1c4206a4adf9f90e9bea5aafa0b
Edbeer <edbeermtn@gmail.com>
2021-09-06 18:42:56 +0700

    complete admin models

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.contrib import admin
 from django import forms
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 
@@ -14,8 +16,45 @@ class PostAdminForm(forms.ModelForm):
 
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
+    prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Tag)
-admin.site.register(Category)
+    list_display = ('id', 'title', 'slug', 'category',
+                    'created_at', 'views', 'get_photo', 'is_published')
+    list_display_links = ('id', 'title', 'slug')
+    list_filter = ('category', 'tags', 'is_published')
+    list_editable = ('is_published',)
+
+    fields = ('title', 'slug', 'content', 'photo', 'get_photo',
+              'category', 'tags', 'views', 'created_at', 'is_published')
+    readonly_fields = ('get_photo', 'views', 'created_at')
+    search_fields = ('title', 'content')
+
+    save_on_top = True
+    save_as = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width=75>')
+
+    get_photo.short_description = 'image'
+
+
+class AdminComment(admin.ModelAdmin):
+    form = PostAdminForm
+    list_display = ('id', 'user', 'post', 'created_at')
+    list_display_links = ('id', 'user')
+    search_fields = ('user', 'post')
+
+
+class AdminTag(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class AdminCategory(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, AdminComment)
+admin.site.register(Tag, AdminTag)
+admin.site.register(Category, AdminCategory)
--- blog/models.py
+++ blog/models.py
@@ -47,10 +47,10 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=1500)
     created_at = models.DateTimeField(auto_now_add=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.user
+        return str(self.user)
 
     class Meta:
         ordering = ['-created_at']
